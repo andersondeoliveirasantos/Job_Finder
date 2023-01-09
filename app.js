7af7eff428fd755cb30e8ce39242e2b9dd057245
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2023-01-09 19:57:12 -0300

    S20-projetoJobFinder.004

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
-const PORT = 3000
+const PORT = 3000;
 
 app.listen(PORT, function() {
-  console.log(`Express is running on the port ${PORT}`)
+  console.log(`Express is running on the port ${PORT}`);
 })
 
+// db connection
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connected with the bank successfully'); // Conectou com o banco com sucesso
+  })
+  .catch(err => {
+    console.log('There was an error connecting', err) // Ocorreu um erro ao conectar
+  });
+
+// Rotes
 app.get('/', (req, res) => {
   res.send("It's working 02")
 })
